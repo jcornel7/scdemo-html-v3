de95ede16790bb40915a6dc87015a7ba0ed434a2
jcornel7 <jcornel7@users.noreply.github.com>
2017-08-20 16:59:55 -0400

    copied

---
+++ www/js/Products.js
@@ -0,0 +1,16 @@
+﻿$(function(){
+    var products =[
+        { value: 'Duracell - AAA Batteries (4-Pack)', price: '5.49' },
+        { value: 'Energizer - N Cell E90 Batteries (2-Pack)', price: '6.03'}, 
+    ];
+  // setup autocomplete function pulling from Names[] array
+  $('#autocomplete').autocomplete({
+    lookup: products,
+    onSelect: function (suggestion) {
+      var thehtml = '<strong>Product Name:</strong> ' + suggestion.value + ' <br> <strong>Price:</strong> ' + suggestion.price;
+      $('#outputcontent').html(thehtml);
+    }
+  });
+  
+
+});
