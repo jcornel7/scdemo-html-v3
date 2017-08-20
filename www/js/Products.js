f6f9623d64276c22fea059bcd7e1def385886d04
jcornel7 <jcornel7@users.noreply.github.com>
2017-08-20 17:28:35 -0400

    test2

--- www/js/Products.js
+++ www/js/Products.js
@@ -1,7 +1,8 @@
 ﻿$(function(){
     var products =[
         { value: 'Duracell - AAA Batteries (4-Pack)', price: '5.49' },
-        { value: 'Energizer - N Cell E90 Batteries (2-Pack)', price: '6.03'}, 
+        { value: 'Fuck Off - N Cell E90 Batteries (2-Pack)', price: '6.03'}, 
+		{ value: 'Dildoes', price: '69.69'},
     ];
   // setup autocomplete function pulling from Names[] array
   $('#autocomplete').autocomplete({
